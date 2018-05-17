d89fb1f1728fa0afc7fe03cff00432cfd30788c0
Michal <you@example.com>
2018-05-17 19:02:14 +0200

    Add form structure and styles

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,28 @@
 class BillsController < ApplicationController
 	def index
 		 @bills = Bill.all 
-	end	
+	end
+
+	def new
+
+	@bill= Bill.new	
+end
+
+def create
+	if Bill.create(bill_params)
+       redirect_to action: :index
+else
+	   render :new
+
+	end
+
+end	
+
+
+
+def bill_params
+	params.require(:bill).permit(:name, :price, :description, :bank_account)
+
+
+end
 end
